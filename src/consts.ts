11eddbdc7336c7f0b2e828e6a7b463c515a956f6
Rob <rob.malyon@gmail.com>
2024-04-27 17:43:43 +0100

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Bimble Aware';
+export const SITE_DESCRIPTION = 'Raising awareness for the meaning of the phrase "Bimble"';
+export const SITE_TAGLINE = 'Always Bimble, Never Bumble';
+export const PAGE_PASSWORD = 'SeeThroughEmu';
